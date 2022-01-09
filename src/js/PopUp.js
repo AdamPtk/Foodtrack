bbb9ff2634baae4e964662fd171d6e0db716cceb
AdamPtk <adam.pietkiewicz12@gmail.com>
2022-01-09 10:04:37 +0100

    Recipe popup async

--- src/js/PopUp.js
+++ src/js/PopUp.js
@@ -1,11 +1,30 @@
-const PopUp = ({recipe}) => {
-    return (
-        <div className="popup">
-            <i class="fas fa-times"></i>
-            <h1>{recipe.strMeal}</h1>
-            {/* <img src={recipe.strMealThumb} alt="MealImg" /> */}
-        </div>
-    );
+import { useState, useEffect } from "react";
+
+const PopUp = ({id}) => {
+    const [choosenRecipe, setChoosenRecipe] = useState(null);
+    
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+        .then(data => setChoosenRecipe(data.meals[0]))
+        .catch(err => console.warn(err))
+    }, [])
+    
+    if (!choosenRecipe) {
+        return (
+            <div className="popup">
+                Ładowanie...
+            </div>
+        ) 
+    } else {
+        return (
+            <div className="popup">
+                <i className="fas fa-times"></i>
+                <h1>{choosenRecipe.strMeal}</h1>
+                <img src={choosenRecipe.strMealThumb} alt="MealImg" />
+            </div>
+        );
+    }
 }
  
 export default PopUp;
--- src/js/Recipe.js
+++ src/js/Recipe.js
@@ -5,18 +5,9 @@ import PopUp from "./PopUp";
 
 const Recipes = () => {
     const [choosenRecipeId, setChoosenRecipeId] = useState(null);
-    const [choosenRecipe, setChoosenRecipe] = useState(null);
     const [showPopUp, setShowPopUp] = useState(false);
 
     const {recipes, handleSelectYourRecipe} = useContext(AppContext);
-
-    useEffect(() => {
-        choosenRecipeId &&
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${choosenRecipeId}`)
-            .then(res => res.json())
-            .then(data => setChoosenRecipe(data.meals[0]))
-            .catch(err => console.warn(err))
-    }, [choosenRecipeId])
 
     const handleChooseRecipe = (id) => {
         setChoosenRecipeId(id);
@@ -25,17 +16,16 @@ const Recipes = () => {
 
     return <>
         {recipes ? recipes.map(recipe => {
-            return <>
+            return (
                 <div className="recipe" key={recipe.idMeal}>
                     <img src={recipe.strMealThumb} alt="MealImg" />
                     <h2>{recipe.strMeal}</h2>
                     <button onClick={() => handleChooseRecipe(recipe.idMeal)}>Check this out!</button>
                     <i className="far fa-heart"></i>
                 </div>
-            </>
+            )
         }) : <h1>No matching results</h1>}
-        {showPopUp  && <PopUp recipe={choosenRecipe}/>}
-        {showPopUp  && console.log(choosenRecipe)}
+        {showPopUp  && <PopUp id={choosenRecipeId}/>}
     </>
 }
  
